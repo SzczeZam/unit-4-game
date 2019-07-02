1b2144484d2c50778bce04defa31a92a54c1a426
Max Szczepaniak <wookslandline@protonmail.com>
2019-07-02 10:30:04 -0400

    Have the javascript working effectively for the game. Need to style and experiment with aesthetic javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,8 @@
 var winDom = $("#wins")
 var lossesDom = $("losses")
 
-
+var wins = 0
+var losses = 0
 
 var targetScore = 0
 var userScore = 0
@@ -31,29 +32,50 @@ var gems = {
     getNewTarget: function (){
         let newValue = this.randNum(19,120)
         targetScore = newValue
-        $("targetScore").text(targetScore)
+        $("#targetScore").text(targetScore)
+        console.log("The targert score is " + targetScore)
     },
-
+    
     reset: function () {
         targetScore = 0
         userScore = 0
-        this.getNewGem()
-        this.getNewTarget()
-    }
+        gems.getNewGem()
+        console.log("new game started")
+        gems.getNewTarget()
+    },
 
 
 }
 
+// initialization
+
 gems.reset()
 
-// LISTENERS
 
 $(document).ready(function() {
+    
+    // LISTENERS
     $(".gemChoice").on("click", function() {
         var value = parseInt($(this).attr("data-value"))
         console.log("Your gem was worth " + value);
         userScore = userScore + value
         $("#userScore").text(userScore)
 
+
+
+        // Logic
+        if (userScore == targetScore){
+            wins++
+            console.log("You won!")
+            $("#wins").text(wins)
+            gems.reset()
+
+        } else if (userScore > targetScore){
+            losses++
+            console.log("You lost...")
+            $("#losses").text(losses)
+            gems.reset()
+        }
     });
 });
+
